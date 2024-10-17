f442614d8e5f5711f0e69d2818138c832cc7e975
Harrish Easwar <harisheaswar@gmail.com>
2024-10-17 16:35:52 +0530

    few changes to be made

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //select the hr line
 const sizeSelector = document.querySelector("#size");
 const quantSelector = document.querySelector("#quantity");
-
-// Deselect/select the drop down hr line
+const menuSelector = document.querySelectorAll(".header-div .menu-list li a");
+const checkBox = document.querySelector("#check");
+const inputBox = document.querySelector(".inputBox");
+// highlight the hr line when select button pressed
 const websiteSelector = document.querySelector(".website-container");
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  let currLink = window.location.pathname;
+
+  currLink = currLink.split("/").pop().trim();
+
+  menuSelector.forEach((element) => {
+    if (element.getAttribute("href") === currLink.trim()) {
+      element.style.borderBottom = "0.15rem solid black";
+      element.style.paddingBottom = "0.7rem";
+    }
+  });
+});
+
 websiteSelector.addEventListener("mousedown", (e) => {
   if (!sizeSelector.contains(e.target) && !quantSelector.contains(e.target)) {
     const hr = document.querySelector("#hr-size");
@@ -26,4 +42,22 @@ websiteSelector.addEventListener("mousedown", (e) => {
     hr.style.height = "0.2rem";
     hr.style.backgroundColor = "black";
   }
+});
+
+checkBox.addEventListener("change", () => {
+  if (checkBox.checked) {
+    menuSelector.forEach((element) => {
+      if (element.style.borderBottom != "") {
+        element.style.borderBottom = "";
+        element.style.fontWeight = "bold";
+        element.style.color = "black";
+      }
+    });
+  }
+});
+
+inputBox.addEventListener("keydown", (e) => {
+  let input = e.target;
+
+  input.style.borderBottom = " 4px solid black";
 });
